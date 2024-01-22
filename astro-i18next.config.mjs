0fdeeadec27fe01f45008a4d0e157ddfa12cb136
Mamad Jadali <jadalimamad@gmail.com>
2024-01-22 03:39:45 +0330

    fr,es

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
-  locales: ["en", "pt", "de", "ru"],
+  locales: ["en", "pt", "de", "ru", "es", "fr"],
 };
